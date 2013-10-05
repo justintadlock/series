990477b4cbcb40c20c173500c3cf87f222154fa7
Justin Tadlock <justin@justintadlock.com>
2013-10-04 22:27:00 -0500

    Restructure plugin files and folders. Uses a standardized class for the main plugin setup.

--- series.php
+++ series.php
@@ -1,104 +1,201 @@
 <?php
 /**
  * Plugin Name: Series
- * Plugin URI: http://justintadlock.com/archives/2009/06/09/series-wordpress-plugin
+ * Plugin URI: http://themehybrid.com/plugins/series
  * Description: Creates a new taxonomy called "Series" that allows you to tie posts together in a series.
- * Version: 0.1.0
+ * Version: 0.2.0-alpha-1
  * Author: Justin Tadlock
  * Author URI: http://justintadlock.com
  *
- * Series is a plugin created to allow users to easily link posts together
- * by using a custom WordPress taxonomy. By using a taxonomy, we're
- * making use of the built-in WordPress functions.  This lets WordPress
- * do all the dirty work, while we just sit back and enjoy adding posts to
- * a series.  This can be particularly useful if you write several posts 
- * spanning the same topic and want them tied together in some way.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the License, 
+ * or (at your option) any later version.
  *
- * @copyright 2009
- * @version 0.1.0
- * @author Justin Tadlock
- * @link http://justintadlock.com/archives/2009/06/09/series-wordpress-plugin
- * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * You should have received a copy of the GNU General Public License along with this program; if not, write 
+ * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
- * @package Series
+ * @package   Series
+ * @version   0.2.0
+ * @since     0.1.0
+ * @author    Justin Tadlock <justin@justintadlock.com>
+ * @copyright Copyright (c) 2009 - 2013, Justin Tadlock
+ * @link      http://themehybrid.com/plugins/series
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-/**
- * Yes, we're localizing the plugin.  This partly makes sure non-English
- * users can use it too.  To translate into your language use the
- * en_EN.po file as as guide.  Poedit is a good tool to for translating.
- * @link http://poedit.net
- *
- * @since 0.1
- */
-load_plugin_textdomain( 'series', false, '/series' );
+final class Series_Plugin {
+
+	/**
+	 * Holds the instance of this class.
+	 *
+	 * @since  0.2.0
+	 * @access private
+	 * @var    object
+	 */
+	private static $instance;
+
+	/**
+	 * Stores the directory path for this plugin.
+	 *
+	 * @since  0.2.0
+	 * @access private
+	 * @var    string
+	 */
+	private $directory_path;
+
+	/**
+	 * Stores the directory URI for this plugin.
+	 *
+	 * @since  0.2.0
+	 * @access private
+	 * @var    string
+	 */
+	private $directory_uri;
+
+	/**
+	 * Plugin setup.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function __construct() {
+
+		/* Set the properties needed by the plugin. */
+		add_action( 'plugins_loaded', array( $this, 'setup' ), 1 );
+
+		/* Internationalize the text strings used. */
+		add_action( 'plugins_loaded', array( $this, 'i18n' ), 2 );
+
+		/* Load the functions files. */
+		add_action( 'plugins_loaded', array( $this, 'includes' ), 3 );
+
+		/* Load the admin files. */
+		add_action( 'plugins_loaded', array( $this, 'admin' ), 4 );
+
+		/* Register widgets. */
+		add_action( 'widgets_init', array( $this, 'register_widgets' ) );
+
+		/* Register activation hook. */
+		register_activation_hook( __FILE__, array( __CLASS__, 'activation' ) );
+	}
+
+	/**
+	 * Defines the directory path and URI for the plugin.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function setup() {
+
+		$this->directory_path = trailingslashit( plugin_dir_path( __FILE__ ) );
+		$this->directory_uri  = trailingslashit( plugin_dir_url(  __FILE__ ) );
+	}
+
+	/**
+	 * Loads the initial files needed by the plugin.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function includes() {
+
+		require_once( "{$this->directory_path}inc/taxonomies.php"            );
+		require_once( "{$this->directory_path}inc/shortcodes.php"            );
+		require_once( "{$this->directory_path}inc/template-tags.php"         );
+		require_once( "{$this->directory_path}inc/series-widget.php"         );
+		require_once( "{$this->directory_path}inc/series-related-widget.php" );
+	}
+
+	/**
+	 * Loads the translation files.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function i18n() {
+
+		/* Load the translation of the plugin. */
+		load_plugin_textdomain( 'series', false, 'series/languages' );
+	}
+
+	/**
+	 * Loads the admin functions and files.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function admin() {
+	}
+
+	/**
+	 * Loads the admin functions and files.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function register_widgets() {
+		register_widget( 'Series_Widget_Series'  );
+		register_widget( 'Series_Widget_Related' );
+	}
+
+	/**
+	 * Method that runs only when the plugin is activated.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public static function activation() {
+
+		/* Get the administrator role. */
+		$role = get_role( 'administrator' );
+
+		/* If the administrator role exists, add required capabilities for the plugin. */
+		if ( !empty( $role ) )
+			$role->add_cap( 'manage_series' );
+	}
+
+	/**
+	 * Returns the instance.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return object
+	 */
+	public static function get_instance() {
+
+		if ( !self::$instance )
+			self::$instance = new self;
+
+		return self::$instance;
+	}
+}
+
+Series_Plugin::get_instance();
+
+/* === DEPRECATED === */
 
 /**
- * Make sure we get the correct directory.
- * @since 0.1
- */
-if ( !defined( 'WP_CONTENT_DIR' ) )
-	define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
-if ( !defined( 'WP_PLUGIN_DIR' ) )
-	define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
-
-/**
- * Define constant paths to the plugin folder.
- * @since 0.1
- */
-define( SERIES_DIR, WP_PLUGIN_DIR . '/series' );
-
-/**
- * Load required files.
- * @since 0.1
- */
-require_once( SERIES_DIR . '/template-tags.php' );
-require_once( SERIES_DIR . '/shortcodes.php' );
-
-/**
- * Creates our taxonomy when 'init' is fired.
- * @since 0.1
- */
-add_action( 'init', 'create_series_taxonomy', 0 );
-
-/**
- * Register series widgets.
- * @since 0.1
- */
-add_action( 'widgets_init', 'series_register_widgets' );
-
-/**
- * Register the series taxonomy.
- * @uses register_taxonomy()
- *
- * @since 0.1
+ * @since      0.1.0
+ * @deprecated 0.2.0
  */
 function create_series_taxonomy() {
-	$structure = get_option( 'permalink_structure' );
-	if ( empty( $structure ) )
-		$args = array( 'hierarchical' => false, 'label' => __('Series', 'series'), 'query_var' => 'series', 'rewrite' => false );
-	else
-		$args = array( 'hierarchical' => false, 'label' => __('Series', 'series'), 'query_var' => 'series', 'rewrite' => array( 'slug' => 'series' ) );
-
-	register_taxonomy( 'series', 'post', $args );
 }
 
 /**
- * Register the Series plugin widgets.
- * @uses register_widget() Registers individual widgets.
- * @link http://codex.wordpress.org/WordPress_Widgets_Api
- *
- * @since 0.1
+ * @since      0.1.0
+ * @deprecated 0.2.0
  */
 function series_register_widgets() {
-	require_once( SERIES_DIR . '/series-widget.php' );
-	require_once( SERIES_DIR . '/series-related-widget.php' );
-	register_widget( 'Series_Widget_Series' );
-	register_widget( 'Series_Widget_Related' );
 }
 
 ?>
