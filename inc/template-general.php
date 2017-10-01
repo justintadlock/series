abd872d3a1ce1378ba954d91cbc63573e8553613
Justin Tadlock <justintadlock@gmail.com>
2017-10-01 12:12:49 -0500

    Introduce the `post_series()` and `get_post_series()` functions.

--- inc/template-general.php
+++ inc/template-general.php
@@ -37,3 +37,52 @@ function in_series( $term, $post = null ) {
 
 	return apply_filters( 'series/in_series', has_term( $series, get_series_taxonomy(), $post ), $term, $post );
 }
+
+/**
+ * Outputs a post's series.
+ *
+ * @since  2.0.0
+ * @access public
+ * @param  array   $args
+ * @return void
+ */
+function post_series( $args = array() ) {
+
+	echo get_post_series( $args );
+}
+
+/**
+ * Wrapper for `get_the_term_list()` for outputting a list of the current
+ * post's series.
+ *
+ * @since  2.0.0
+ * @access public
+ * @param  array   $args
+ * @return string
+ */
+function get_post_series( $args = array() ) {
+
+	$html = '';
+
+	$defaults = array(
+		'post_id'    => get_the_ID(),
+		'text'       => '%s',
+		'before'     => '',
+		'after'      => '',
+		'wrap'       => '<span %s>%s</span>',
+		// Translators: Separates tags, categories, etc. when displaying a post.
+		'sep'        => _x( ', ', 'taxonomy terms separator', 'series' )
+	);
+
+	$args = wp_parse_args( $args, $defaults );
+
+	$terms = get_the_term_list( $args['post_id'], get_series_taxonomy(), '', $args['sep'], '' );
+
+	if ( $terms ) {
+		$html .= $args['before'];
+		$html .= sprintf( $args['wrap'], 'class="entry-terms entry-series"', sprintf( $args['text'], $terms ) );
+		$html .= $args['after'];
+	}
+
+	return $html;
+}
